e19b8e78e4f6e938a24337be2e687745313917e2
Nitish Patel <nitishpatel1234@gmail.com>
2020-12-31 16:22:23 +0530

    Developing Button

--- Watershed_Identification_tool.py
+++ Watershed_Identification_tool.py
@@ -4,6 +4,10 @@ from qgis.core import *
 from qgis.gui import *
 from qgis.PyQt.QtCore import Qt
 import sys
+import qgis
+
+import processing
+from processing.core.Processing import Processing
 
 
 class PointTool(QgsMapTool):
@@ -23,12 +27,15 @@ class PointTool(QgsMapTool):
 class MyCanvas(QgsMapCanvas):
     def __init__(self, dem_path):
         super(MyCanvas, self).__init__()
+        self.setWindowTitle("Watershed Tool")
+
         # Initialize paths and all
-        self.csv_file_path = r"store_coordinates.csv"
+        self.csv_file_path = r"temp_dir\store_coordinates.csv"
         self.dem_path = dem_path
 
         self.pointLayer = None
         self.dem_layer = None
+        self.long, self.lat = 0, 0
 
         # Initialize the functionality of the app
         self.initUI()
@@ -44,8 +51,9 @@ class MyCanvas(QgsMapCanvas):
         self.setLayers([self.dem_layer])
 
     def initUI(self):
-        button = QtWidgets.QPushButton(self)
-        button.setText("Test Button")
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText("Test Button")
+        self.button.released.connect(self.generateBasin())
 
         label = QtWidgets.QLabel(self)
         label.setText(self.dem_path.split("\\")[-1])
@@ -56,22 +64,33 @@ class MyCanvas(QgsMapCanvas):
 
     def getCoordinates(self, point):
         print("Hello")
-        long, lat = list(point)
-        to_write = "lat,long,nn\n{},{},name".format(lat, long)
+        self.long, self.lat = list(point)
+        to_write = "lat,long,nn\n{},{},name".format(self.lat, self.long)
         with open(self.csv_file_path, 'w') as csv_file:
             csv_file.write(to_write)
 
-        uri = "file:///" + sys.path[0] + "/store_coordinates.csv?delimiter=%s&xField=%s&yField=%s&crs=%s" % (
-            ",", "long", "lat", "epsg:4326")
+        uri = "file:///{}/{}?delimiter={}&xField={}&yField={}&crs={}".format(sys.path[0], self.csv_file_path,
+                                                                             ",", "long", "lat", "EPSG:32643")
         self.pointLayer = QgsVectorLayer(uri, 'New CSV', 'delimitedtext')
         if self.pointLayer.isValid():
             print("Point Loaded and Created")
 
-        # symbol = QgsSymbol.defaultSymbol(pointLayer.geometryType())
-        # symbol = renderer.symbol()
-        # symbol.setColor(Qcolor.fromRgb(255,128,0))
-
         self.setLayers([self.pointLayer, self.dem_layer])
+        # self.refreshAllLayers()
+        # self.generateBasin()
+
+    def generateBasin(self):
+        basin_path = 'temp_dir/hehl.tif'
+        coordinate_string = '{},{} [EPSG:32643]'.format(self.long, self.lat)
+        processing.run("grass7:r.water.outlet", {'input': dem_path,
+                                                 'coordinates': coordinate_string,
+                                                 'output': basin_path,
+                                                 'GRASS_REGION_PARAMETER': None, 'GRASS_REGION_CELLSIZE_PARAMETER': 0,
+                                                 'GRASS_RASTER_FORMAT_OPT': '', 'GRASS_RASTER_FORMAT_META': ''})
+        basin_raster = QgsRasterLayer(basin_path, "basin")
+        if basin_raster.isValid():
+            print("Basin Raster Created")
+        self.setLayers([basin_raster, self.pointLayer, self.dem_layer])
         self.refreshAllLayers()
 
 
@@ -82,10 +101,14 @@ qgs = QgsApplication([], True)
 
 # Load providers
 qgs.initQgis()
+Processing.initialize()
 
 # Create canvas and show
-canvas = MyCanvas(dem_path=r"D:\Nitish\1220_Dec\5_DEM_to_Stream_micro\Input Data\mndrayal_dem.img")
+dem_path = sys.path[0] + r"\example_raster\dem.img"
+canvas = MyCanvas(dem_path=dem_path)
 canvas.show()
+
+
 
 # qgs.exitQgis()
 sys.exit(qgs.exec_())
---
+++ thrash/test.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("Hello")
+for path in sys.path:
+    print(path)
